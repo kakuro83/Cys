fe8add2501263b6947258d020349220f028c9277
kakuro83 <kahicer@gmail.com>
2025-03-26 23:12:55 -0500

    Update Termociclador.py

--- Termociclador.py
+++ Termociclador.py
@@ -18,7 +18,7 @@ col1, col2 = st.columns([1, 9])
 
 # --- INTERFAZ DE USUARIO ---
 with col1:
-    st.image(icono, width=500)
+    st.image(icono, width=50)
 with col2:
     st.markdown("<h1 style='padding-top: 10px;'>Termociclador Virtual</h1>", unsafe_allow_html=True)
 
